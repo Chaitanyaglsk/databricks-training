46653baa500643cd7369341f0f3736ba65ae3d2d
Chaitanya GLSK <chaitanyaglsk@gmail.com>
2026-05-07 18:08:21 +0530

    Update queries.sql

--- week-1/day-2/queries.sql
+++ week-1/day-2/queries.sql
@@ -29,50 +29,50 @@ department_id	no_of_employees
 20). select department_id ,avg(salary) as average_salary from Employee group by department_id;
 
 department_id			average_salary
-		null					  48000.0
-    1								65000.0
-    2								55000.0
-    3								50000.0
-    4								61500.0
+	null					48000.0
+    1						65000.0
+    2						55000.0
+    3						50000.0
+    4						61500.0
 
 
 21). select department_id ,sum(salary) as total_salary from Employee group by department_id;
 	
 department_id			total_salary
-		null	         	48000.0
-			1							195000.0
-			2							110000.0
-			3							100000.0
-			4							123000.0
+	null	         		48000.0
+	1						195000.0
+	2						110000.0
+	3						100000.0
+	4						123000.0
 
 
 22).select department_id ,avg(age) as average_age from Employee group by department_id;
 
 department_id		average_age
-		null					32.0
-			1						33.3333
-			2						31.5
-			3						32.5
-			4						34.0
+	null				32.0
+	1					33.3333
+	2					31.5
+	3					32.5
+	4					34.0
 
 
 23).select year(hire_date) ,count(*) as no_of_employees from Employee group by year(hire_date);
 
 year(hire_date)	no_of_employees
-			2018	      	2
-			2019					2
-			2020					2
-			2021					3
-			2022					1
+		2018	      	2
+		2019			2
+		2020			2
+		2021			3
+		2022			1
 
 24).select department_id, max(salary) as Highest_salary from Employee group by department_id;
 
 department_id		Highest_salary
     null					48000.0
-      1						80000.0
-			2						60000.0
-			3						55000.0
-			4						70000.0
+    1						80000.0
+	2						60000.0
+	3						55000.0
+	4						70000.0
 	
 
 25).select department_id from Employee group by department_id order by avg(salary) desc limit 1;
